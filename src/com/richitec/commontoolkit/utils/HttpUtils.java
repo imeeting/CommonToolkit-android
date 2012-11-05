fbf1de7978f6ef555cfcf5f2e0889265dfe363fa
starkingwx <starkingwx@gmail.com>
2012-11-05 16:08:30 +0800

    add HTTPS support for HTTPUtil
    update websocket library

--- src/com/richitec/commontoolkit/utils/HttpUtils.java
+++ src/com/richitec/commontoolkit/utils/HttpUtils.java
@@ -22,6 +22,7 @@ import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -68,6 +69,7 @@ public class HttpUtils {
 		SchemeRegistry registry = new SchemeRegistry();
 		registry.register(new Scheme("http", PlainSocketFactory
 				.getSocketFactory(), 80));
+		registry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
 		ClientConnectionManager cm = new ThreadSafeClientConnManager(
 				_httpParameters, registry);
 
